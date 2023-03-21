5c5c5aec8383e4878396b7bc175a1465ee4ede04
Torsten Grote <t@grobox.de>
2023-03-21 13:47:53 -0300

    [wallet] uppercase IBAN for deposits

--- wallet/src/main/java/net/taler/wallet/deposit/MakeDepositComposable.kt
+++ wallet/src/main/java/net/taler/wallet/deposit/MakeDepositComposable.kt
@@ -22,9 +22,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -97,7 +97,7 @@ fun MakeDepositComposable(
             value = iban,
             enabled = !state.showFees,
             onValueChange = { input ->
-                iban = input
+                iban = input.uppercase()
             },
             isError = iban.isBlank(),
             label = {
